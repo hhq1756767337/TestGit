077275a6cac4f9b4054e31cbf39af718d09c00b5
hhq1756767337 <hhq0796@163.com>
2019-12-18 21:54:05 +0800

    新建了前端

---
+++ src/main/java/cn/demo/mydemo/controller/LoginController.java
@@ -0,0 +1,16 @@
+package cn.demo.mydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController
+{
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login()
+    {
+        return "hello Word!";
+    }
+}
