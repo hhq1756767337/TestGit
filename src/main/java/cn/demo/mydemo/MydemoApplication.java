c5304b36326809445616d0e71a4df1c0202f899b
hhq1756767337 <hhq0796@163.com>
2019-12-18 21:59:49 +0800

    新建了Controller

--- src/main/java/cn/demo/mydemo/MydemoApplication.java
+++ src/main/java/cn/demo/mydemo/MydemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MydemoApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(MydemoApplication.class, args);
