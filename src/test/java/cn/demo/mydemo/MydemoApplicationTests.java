aa2e5a78bce5a77990ef2710d5b3e0bed4b95f28
hhq1756767337 <hhq0796@163.com>
2019-12-18 21:42:14 +0800

    first commit

---
+++ src/test/java/cn/demo/mydemo/MydemoApplicationTests.java
@@ -0,0 +1,15 @@
+package cn.demo.mydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
